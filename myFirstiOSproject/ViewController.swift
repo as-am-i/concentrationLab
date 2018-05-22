ac7922aa8598212b096a8d9d42bdb3bd380cf8f2
Asami Tanii <g033.9521@gmail.com>
2018-05-22 13:25:58 -0700

    Initial Commit

---
+++ myFirstiOSproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFirstiOSproject
+//
+//  Created by 谷井朝美 on 2018-05-22.
+//  Copyright © 2018 Asami Tanii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
